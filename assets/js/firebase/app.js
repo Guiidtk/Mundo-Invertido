9335d4714918ac47d34e6ff7594f4ab014ebdaaa
Guiidtk <guilhermealbuquerque.senai@gmail.com>
2022-08-29 12:35:29 -0300

    first commit

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,18 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBxN7P8YCHqRu7GKTkQPCdFznwEI93gY0w",
+    authDomain: "mundo-invertido-81f20.firebaseapp.com",
+    projectId: "mundo-invertido-81f20",
+    storageBucket: "mundo-invertido-81f20.appspot.com",
+    messagingSenderId: "1082810692817",
+    appId: "1:1082810692817:web:7b13f6db0aceeeb9b681f0"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+export default app
---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+import { getHellfireClubeSubscriptions, subscribeToHellfireClube } from "./firebase/hellfire-clube.js"
+
+const txtName = document.getElementById('txtName')
+const txtEmail  = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
+
+const btncSubscribe = document.getElementById('btncSubscribe')
+
+const subscriptionsList = document.getElementById('subscriptions')
+
+btncSubscribe.addEventListener('click', async () => {
+    const subscription = {
+        name: txtName.value,
+        email: txtEmail.value,
+        level: txtLevel.value,
+        Character: txtCharacter.value,
+     }
+
+        const subscriptionID = await subscribeToHellfireClube(subscription)
+    
+        txtName.value = ''
+        txtEmail.value = ''
+        txtLevel.value = ''
+        txtCharacter.value = ''
+
+        alert('Inscrito com sucesso: ${subscriptionID}')
+})
+
+async function loadData() {
+const subscriptions = await getHellfireClubeSubscriptions() 
+
+subscriptionsList.innerHTML = subscriptions.map(sub => '
+<li>
+     ${sub.name}
+   </li>
+').join('') 
+console.log(subscriptions)
+}
+
+loadData()
+
